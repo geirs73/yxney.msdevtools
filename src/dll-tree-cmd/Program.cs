c3df938004ab97b87f524ce005df20cd4c39b08b
Geir Sørensen <geir.sorensen@dnvgl.com>
2020-12-04 00:02:37 +0100

    Program more or less working enough

--- src/dll-tree-cmd/Program.cs
+++ src/dll-tree-cmd/Program.cs
@@ -1,14 +1,47 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
 
 namespace DllTreeCmd
 {
-    class program
+    class Program
     {
-        // static void Main(string[] args)
-        /// <param name="number">An option whose argument is parsed as an int</param>
-        static void Main(int number = 42)
+        // this program uses dragonfruit magic, because convenience
+
+        public enum OutputFormat
         {
-            Console.WriteLine($"Hello World! {number}");
+            csv,
+            txt
+        }
+
+        // static void Main(string[] args)
+        /// <param name="path">Path to folder</param>
+        /// <param name="pattern"></param>
+        /// <param name="excludeDir">File filter</param>
+        /// <param name="format">File filter</param>
+        static void Main(string path = ".", string pattern = "*.*", string excludeDir = "", OutputFormat format=OutputFormat.csv)
+        {
+            var files = Directory.GetFiles(path, pattern, SearchOption.AllDirectories);
+            foreach (var f in files)
+            {
+                var dirs = f.Split(Path.DirectorySeparatorChar).ToList();
+                var evilDirs = from d in dirs where d.Equals(excludeDir, StringComparison.OrdinalIgnoreCase) select d;
+                if (!string.IsNullOrEmpty(excludeDir) && dirs.Any())
+                {
+                    var version = FileVersionInfo.GetVersionInfo(f);
+                    switch (format)
+                    {
+                        case OutputFormat.csv:
+                        Console.Out.WriteLine($"{f};{version.FileVersion}");
+                        break;
+                        default:
+                        Console.Out.WriteLine($"File={f}\tVersion={version.FileVersion}");
+                        break;
+                    }
+
+                }
+            }
         }
     }
 }
