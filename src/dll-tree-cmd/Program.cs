9687749c76a4693b99281e5f4aec7897a6f67b85
Geir Sørensen <geir.sorensen@dnvgl.com>
2020-12-03 23:19:25 +0100

    Added DllTree command line exe

---
+++ src/dll-tree-cmd/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DllTreeCmd
+{
+    class program
+    {
+        // static void Main(string[] args)
+        /// <param name="number">An option whose argument is parsed as an int</param>
+        static void Main(int number = 42)
+        {
+            Console.WriteLine($"Hello World! {number}");
+        }
+    }
+}
