1a1f4d1b7302f2f1046d13f261744b3f66c28c53
Geir Sørensen <geir.sorensen@dnvgl.com>
2020-12-04 10:47:16 +0100

    Quite working version :-)

---
+++ src/DllTreeCmd/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DllTreeCmd
+{
+    internal class Program
+    {
+        // this program uses dragonfruit magic, because convenience
+
+        public enum OutputFormat
+        {
+            Csv,
+            Txt,
+            Json
+        }
+
+        /// <param name="path">Path to folder</param>
+        /// <param name="pattern">File pattern to match</param>
+        /// <param name="excludeDirs">Semi-colon separated list of names to exclude</param>
+        /// <param name="format">Output format, only CSV for now</param>
+        public static void Main(string path = ".", string pattern = "*.*", string excludeDirs = "obj", OutputFormat format = OutputFormat.Csv)
+        {
+            switch (format)
+            {
+                case OutputFormat.Csv: break;
+                default:
+                    throw new Exception("Only CSV format is supported at this time");
+            }
+            var dir = new DirectoryInfo(path);
+            var files = dir.GetFiles(pattern, SearchOption.AllDirectories);
+
+            var exclusions = (from d in excludeDirs.Split(';') select d.ToUpperInvariant()).ToList();
+            var filtered1 = (from f in files where !exclusions.Any(f.FullName.ToUpperInvariant().Contains) select f).ToList();
+
+            Console.Out.WriteLine("sep=;");
+            Console.Out.WriteLine($"Path;Version;LastWrittenTime");
+            foreach (FileInfo f in filtered1)
+            {
+                var version = FileVersionInfo.GetVersionInfo(f.FullName);
+                Console.Out.WriteLine($"{f};{version.FileVersion};{f.LastAccessTime:O}");
+            }
+        }
+    }
+}
